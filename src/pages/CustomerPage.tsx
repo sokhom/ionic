b8f66860bb7d1477a23b24131c83e3345c1f2e62
haysokhom@nagaworld.com <haysokhom@nagaworld.com>
2020-05-08 18:02:18 +0700

    first commit

---
+++ src/pages/CustomerPage.tsx
@@ -0,0 +1,187 @@
+import React, { useState, useRef } from 'react';
+
+import { IonToolbar, IonContent, IonPage, IonButtons, 
+  IonTitle, IonMenuButton, IonSegment, IonSegmentButton, IonButton, IonIcon, IonSearchbar, 
+  IonRefresher, IonRefresherContent, IonToast, IonModal, IonHeader, 
+  IonBackButton,
+  IonPopover,
+  getConfig 
+} from '@ionic/react';
+import { options, search, ellipsisHorizontal } from 'ionicons/icons';
+
+import SessionList from '../components/SessionList';
+import SessionListFilter from '../components/SessionListFilter';
+import './CustomerPage.scss'
+
+import ShareSocialFab from '../components/ShareSocialFab';
+
+import * as selectors from '../data/selectors';
+import { connect } from '../data/connect';
+import { setSearchText } from '../data/sessions/sessions.actions';
+import { Schedule } from '../models/Schedule';
+import AboutPopover from '../components/AboutPopover';
+
+interface OwnProps { }
+
+interface StateProps {
+  schedule: Schedule;
+  favoritesSchedule: Schedule;
+  mode: 'ios' | 'md'
+}
+
+interface DispatchProps {
+  setSearchText: typeof setSearchText;
+}
+
+type CustomerPageProps = OwnProps & StateProps & DispatchProps;
+
+const CustomerPage: React.FC<CustomerPageProps> = ({ favoritesSchedule, schedule, setSearchText, mode }) => {
+  const [segment, setSegment] = useState<'all' | 'favorites'>('all');
+  const [showSearchbar, setShowSearchbar] = useState<boolean>(false);
+  const [showFilterModal, setShowFilterModal] = useState(false);
+  const ionRefresherRef = useRef<HTMLIonRefresherElement>(null);
+  const [showCompleteToast, setShowCompleteToast] = useState(false);
+  const [showPopover, setShowPopover] = useState(false);
+  const [popoverEvent, setPopoverEvent] = useState();
+
+  const pageRef = useRef<HTMLElement>(null);
+
+  const ios = mode === 'ios';
+
+  const presentPopover = (e: React.MouseEvent) => {
+    setPopoverEvent(e.nativeEvent);
+    setShowPopover(true);
+  };
+
+  const doRefresh = () => {
+    setTimeout(() => {
+      ionRefresherRef.current!.complete();
+      setShowCompleteToast(true);
+    }, 2500)
+  };
+
+  return (
+    <IonPage ref={pageRef} id="customer-page">
+      <IonHeader translucent={true}>
+        <IonToolbar>
+          {!showSearchbar &&
+            <IonButtons slot="start">
+              <IonMenuButton />
+            </IonButtons>
+          }
+          {ios &&
+            <IonSegment value={segment} onIonChange={(e) => setSegment(e.detail.value as any)}>
+              <IonSegmentButton value="all">
+                All
+              </IonSegmentButton>
+              <IonSegmentButton value="favorites">
+                Favorites
+              </IonSegmentButton>
+            </IonSegment>
+          }
+          {!ios && !showSearchbar &&
+            <IonTitle>Customer</IonTitle>
+          }
+          {showSearchbar &&
+            <IonSearchbar showCancelButton="always" placeholder="Search" onIonChange={(e: CustomEvent) => setSearchText(e.detail.value)} onIonCancel={() => setShowSearchbar(false)}></IonSearchbar>
+          }
+
+          <IonButtons slot="end">
+            {!ios && !showSearchbar &&
+              <IonButton onClick={() => setShowSearchbar(true)}>
+                <IonIcon slot="icon-only" icon={search}></IonIcon>
+              </IonButton>
+            }
+            {!showSearchbar &&
+              <IonButton onClick={presentPopover} >
+                {mode === 'ios' ? 'Filter' : <IonIcon icon={ellipsisHorizontal} slot="icon-only" />}
+              </IonButton>
+            }
+          </IonButtons>
+        </IonToolbar>
+
+        {!ios &&
+          <IonToolbar>
+            <IonSegment value={segment} onIonChange={(e) => setSegment(e.detail.value as any)}>
+              <IonSegmentButton value="all">
+                All
+              </IonSegmentButton>
+              <IonSegmentButton value="favorites">
+                Favorites
+              </IonSegmentButton>
+            </IonSegment>
+          </IonToolbar>
+        }
+      </IonHeader>
+
+      <IonContent fullscreen={true}>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Customer</IonTitle>
+          </IonToolbar>
+          <IonToolbar>
+            <IonSearchbar placeholder="Search" onIonChange={(e: CustomEvent) => setSearchText(e.detail.value)}></IonSearchbar>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonRefresher slot="fixed" ref={ionRefresherRef} onIonRefresh={doRefresh}>
+          <IonRefresherContent />
+        </IonRefresher>
+
+        <IonToast
+          isOpen={showCompleteToast}
+          message="Refresh complete"
+          duration={2000}
+          onDidDismiss={() => setShowCompleteToast(false)}
+        />
+
+        <SessionList
+          schedule={schedule}
+          listType={segment}
+          hide={segment === 'favorites'}
+        />
+        <SessionList
+          // schedule={schedule}
+          schedule={favoritesSchedule}
+          listType={segment}
+          hide={segment === 'all'}
+        />
+      </IonContent>
+
+      <IonModal
+        isOpen={showFilterModal}
+        onDidDismiss={() => setShowFilterModal(false)}
+        swipeToClose={true}
+        presentingElement={pageRef.current!}
+        cssClass="session-list-filter"
+      >
+        <SessionListFilter
+          onDismissModal={() => setShowFilterModal(false)}
+        />
+      </IonModal>    
+
+      <IonPopover
+        isOpen={showPopover}
+        event={popoverEvent}
+        onDidDismiss={() => setShowPopover(false)}
+      >
+        <AboutPopover dismiss={() => setShowPopover(false)} />
+      </IonPopover>
+
+      <ShareSocialFab />
+
+    </IonPage>
+  );
+};
+
+export default connect<OwnProps, StateProps, DispatchProps>({
+  mapStateToProps: (state) => ({
+    schedule: selectors.getSearchedSchedule(state),
+    favoritesSchedule: selectors.getGroupedFavorites(state),
+    mode: getConfig()!.get('mode')
+  }),
+  mapDispatchToProps: {
+    setSearchText
+  },
+  component: React.memo(CustomerPage)
+});
